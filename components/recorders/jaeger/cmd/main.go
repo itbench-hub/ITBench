21a1537b248ebdcd9738a2f1e41692a97d149c9a
Gerard Vanloo <gerard.vanloo@ibm.com>
2026-09-16 21:29:01 -0400

    chore: move prometheus and jaeger recorder to statefulset design
    Signed-off-by: Gerard Vanloo <gerard.vanloo@ibm.com>

--- components/recorders/jaeger/cmd/main.go
+++ components/recorders/jaeger/cmd/main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/itbench-hub/ITBench/components/recorders/jaeger/internal/recorder"
 )
@@ -23,8 +27,37 @@ func main() {
 	}
 	outputDir := filepath.Join(homeDir, "records")
 
-	if err := recorder.Run(context.Background(), endpoint, outputDir); err != nil {
-		slog.Error("recorder failed", "err", err)
-		os.Exit(1)
+	intervalSec := 300
+	if val := os.Getenv("INTERVAL_SECONDS"); val != "" {
+		if parsed, err := strconv.Atoi(val); err == nil && parsed > 0 {
+			intervalSec = parsed
+		} else {
+			slog.Warn("invalid INTERVAL_SECONDS, using default", "value", val, "default", intervalSec)
+		}
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	slog.Info("starting jaeger recorder daemon", "interval_seconds", intervalSec, "output_dir", outputDir)
+
+	if err := recorder.Run(ctx, endpoint, outputDir); err != nil {
+		slog.Error("initial recorder run failed", "err", err)
+	}
+
+	ticker := time.NewTicker(time.Duration(intervalSec) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			slog.Info("shutting down jaeger recorder daemon")
+			return
+		case <-ticker.C:
+			slog.Info("running scheduled scrape")
+			if err := recorder.Run(ctx, endpoint, outputDir); err != nil {
+				slog.Error("scheduled recorder run failed", "err", err)
+			}
+		}
 	}
 }
